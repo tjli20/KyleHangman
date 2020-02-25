ae86e0c7fb454d83970202e88a1b1d0c45e1894e
krlusi20 <60932596+krlusi20@users.noreply.github.com>
2020-02-25 10:12:43 -0500

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,37 @@
+''' Initialize global variables'''
+word = ""
+letter = ""
+updatedSpaces = []
+
+def intialize():
+    ''' List = ["Dog","Cat","Tree"]
+    random.choice(
+    '''
+    print("print word")
+    
+
+def getLetter():
+    '''Out letter from user'''
+    global letter
+    
+def checkLetter():
+    '''tests if letter in secret, 
+    if True updates undatedSpaces displays 
+        updatedSoaces, calls checkIfWon
+    if False displays updatedSpaces, updates lives, calls 
+        getLetter
+    '''
+    global updatedSpaces
+    
+def checkIfWon():
+    '''outputs a message if won or 
+        calls getLetter if not'''
+    print()
+
+def main():
+    '''Starts the program'''
+    intialize()
+    getLetter()
+    checkLetter()
+
+main()
